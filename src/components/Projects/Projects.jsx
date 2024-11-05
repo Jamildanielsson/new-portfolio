cd735191b5537b1073a2e97d7e3f6056bd3f6a19
Jamildanielsson <114480495+Jamildanielsson@users.noreply.github.com>
2024-11-05 09:45:15 +0100

    feat: indentation and removal of comments

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,52 +1,53 @@
 import "./projects.css";
-import { useState } from 'react';
-import weatherApp from '../../assets/weather.png';
-import calculatorApp from '../../assets/calculator.png';
-import todoApp from '../../assets/todo.png';
-import dictionaryApp from '../../assets/dictionary.png';
+import { useState } from "react";
+import weatherApp from "../../assets/weather.png";
+import calculatorApp from "../../assets/calculator.png";
+import todoApp from "../../assets/todo.png";
+import dictionaryApp from "../../assets/dictionary.png";
 
 const Projects = () => {
   const [hovered, setHovered] = useState(null);
 
   const handleMouseEnter = (index) => {
     setHovered(index);
-    console.log('hovrar över');
-    
+    console.log("hovrar över");
   };
 
   const handleMouseLeave = () => {
     setHovered(null);
-    console.log('slutar hovra')
+    console.log("slutar hovra");
   };
 
   const projects = [
     {
       image: weatherApp,
-      description: 'Weather App: An app that uses OpenWeatherAPI.',
-      tags: ['React.js', 'API', 'HTML5', 'CSS', 'Javascript'],
-      link: 'https://weather-checkcheck.netlify.app/'
+      description: "Weather App: An app that uses OpenWeatherAPI.",
+      tags: ["React.js", "API", "HTML5", "CSS", "Javascript"],
+      link: "https://weather-checkcheck.netlify.app/",
     },
     {
       image: calculatorApp,
-      description: 'Calculator App: Use it on the go',
-      tags: ['HTML5', 'Typescript', 'React.js']
+      description: "Calculator App: Use it on the go",
+      tags: ["HTML5", "Typescript", "React.js"],
     },
     {
       image: todoApp,
-      description: 'To Do App: Makes your everyday easier.',
-      tags: ['HTML5', 'TailwindCSS', 'React.js']
+      description: "To Do App: Makes your everyday easier.",
+      tags: ["HTML5", "TailwindCSS", "React.js"],
     },
     {
       image: dictionaryApp,
-      description: 'Dictionary App: Hard word? No problem.',
-      tags: ['React.js', 'HTML5', 'SCSS']
+      description: "Dictionary App: Hard word? No problem.",
+      tags: ["React.js", "HTML5", "SCSS"],
     },
   ];
 
   return (
     <div className="projects-wrapper">
       <div className="projects-list">
-        <div className="projects-headline"><h3>Hover over a project to learn more.</h3></div>
+        <div className="projects-headline">
+          <h3>Hover over a project to learn more.</h3>
+        </div>
         {projects.map((project, index) => (
           <div
             key={index}
@@ -54,16 +55,26 @@ const Projects = () => {
             onMouseEnter={() => handleMouseEnter(index)}
             onMouseLeave={handleMouseLeave}
           >
-            <img src={project.image} alt="Project image" className="project-img"/>
+            <img
+              src={project.image}
+              alt="Project image"
+              className="project-img"
+            />
             {hovered === index && (
               <div className="projects-overlay">
                 <p className="project-desc">{project.description}</p>
                 <div className="project-tags">
                   {project.tags.map((tag, tagIndex) => (
-                    <span key={tagIndex} className="project-tag">{tag}</span>
+                    <span key={tagIndex} className="project-tag">
+                      {tag}
+                    </span>
                   ))}
                 </div>
-                <a href={project.link} target="_blank" rel="noopener noreferrer">
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button className="watch-live-button">Watch live</button>
                 </a>
               </div>
@@ -73,6 +84,6 @@ const Projects = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Projects;
